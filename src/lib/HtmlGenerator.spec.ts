c93ea7760c8facb1e93cc3a7908b5b96992cb83f
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-11-12 00:43:20 +0200

    Removed testing template

--- src/lib/HtmlGenerator.spec.ts
+++ src/lib/HtmlGenerator.spec.ts
@@ -6,43 +6,46 @@ const {JSDOM} = require('jsdom');
 import {generate} from './HtmlGenerator';
 import * as utils from './utils';
 
-const testTemplateName = 'test';
-const testTemplatePath = path.resolve('src', 'templates', `${testTemplateName}.ejs`);
-
-test('testing template should exist', async t => {
-  t.truthy(await utils.fileExists(testTemplatePath));
-});
+const templateString = `
+<html>
+  <head>
+    <title><%= params.title %></title>
+  </head>
+</html>`;
 
 test.beforeEach (async t => {
   t.context = {...t.context,
-    testOutFilePath: path.resolve('temp', `${utils.generateRandomString()}.html`),
-    templateAsString: await utils.readFileContents(testTemplatePath)
+    testTemplatePath: await utils.writeFileContents(
+      path.resolve('temp', `${utils.generateRandomString()}.ejs`),
+      templateString
+    ),
+    testOutFilePath: path.resolve('temp', `${utils.generateRandomString()}.html`)
   };
 });
 
 test.afterEach.always(async t => {
-  const {testOutFilePath} = t.context as any;
+  const {testOutFilePath, testTemplatePath} = t.context as any;
   try {
     fs.unlinkSync(testOutFilePath);
-  } catch(e) {
+    fs.unlinkSync(testTemplatePath);
+  }catch(e){
     t.log(e);
   }
 });
 
 test('should generate html file', async t => {
-  const {testOutFilePath} = t.context as any;
-  await generate(testTemplateName, testOutFilePath)
+  const {testOutFilePath, testTemplatePath} = t.context as any;
+  await generate(testTemplatePath, testOutFilePath)
   t.is(await utils.fileExists(testOutFilePath), true);
 });
 
 test('should apply template parameters', async t => {
-  const {testOutFilePath, templateAsString} = t.context as any;
+  const {testOutFilePath, testTemplatePath} = t.context as any;
   const title = 'TEST TITLE';
   const params = {title};
-  await generate(testTemplateName, testOutFilePath, params)
+  await generate(testTemplatePath, testOutFilePath, params)
   const generatedHtmlString = await utils.readFileContents(testOutFilePath);
-  t.not(generatedHtmlString, templateAsString);
+  t.not(generatedHtmlString, templateString);
   const window = new JSDOM(generatedHtmlString).window;
   t.is(window.document.title, title);
 });
-
--- src/lib/HtmlGenerator.ts
+++ src/lib/HtmlGenerator.ts
@@ -1,14 +1,9 @@
 import ejs from 'ejs';
-import path from 'path';
+// import path from 'path';
 import * as utils from './utils';
 
-const getTemplatePath = (templateName: string) => path.resolve(
-  'src', 'templates',
-  `${templateName}.ejs`
-);
-
-export async function generate(templateName: string, outPath: string, params: Object = {}) {
-  const templateString = await utils.readFileContents(getTemplatePath(templateName));
+export async function generate(templatePath: string, outPath: string, params: Object = {}) {
+  const templateString = await utils.readFileContents(templatePath);
   const htmlString = ejs.render(templateString, {params});
   await utils.writeFileContents(outPath, htmlString);
 }
