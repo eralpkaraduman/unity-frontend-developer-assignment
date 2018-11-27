47c78b249e5606b6b1250af1028d96ee0a594a23
Eralp <eralpkaraduman@gmail.com>
2018-11-27 17:33:00 +0200

    Implemented gallery interactions

--- src/templates/interstitial-carousel-ad-unit/script.spec.ts
+++ src/templates/interstitial-carousel-ad-unit/script.spec.ts
@@ -45,8 +45,39 @@ test.afterEach.always(async t => {
   }
 });
 
-test('should test transpiled script', async t => {
+test('should calculate target index', async t => {
   const { rewiredScript } = t.context as any;
-  const func = rewiredScript.__get__('func');
-  t.is(func(1, 2), 3);
+  const calculateTargetIndex = rewiredScript.__get__('calculateTargetIndex');
+  t.is(calculateTargetIndex(0, +1, 3), 1);
+  t.is(calculateTargetIndex(0, -1, 3), 2);
+  t.is(calculateTargetIndex(0, +2, 3), 2);
+  t.is(calculateTargetIndex(0, +3, 3), 0);
+  t.is(calculateTargetIndex(0, -2, 3), 1);
+  t.is(calculateTargetIndex(0, -3, 3), 0);
+  t.is(calculateTargetIndex(0, 0, 0), 0);
+  t.is(calculateTargetIndex(2, +1, 3), 0);
+  t.is(calculateTargetIndex(0, -1, 0), 0);
+  t.is(calculateTargetIndex(0, +1, 0), 0);
+  t.is(calculateTargetIndex(-1, 0, 0), -1);
+  t.is(calculateTargetIndex(NaN, 0, 0), NaN);
+  t.is(calculateTargetIndex(0, NaN, 0), 0);
+  t.is(calculateTargetIndex(0, 0, NaN), 0);
+  t.is(calculateTargetIndex(null, 0, 0), null);
+  t.is(calculateTargetIndex(0, null, 0), 0);
+  t.is(calculateTargetIndex(0, 0, null), 0);
+});
+
+test('should strip index from dot button element', async t => {
+  const { rewiredScript } = t.context as any;
+  const stripIndexFromDotButtonElement = rewiredScript.__get__('stripIndexFromDotButtonElement');
+  t.is(stripIndexFromDotButtonElement({id: 'carousel-navigation-dot-index-0'}), 0);
+  t.is(stripIndexFromDotButtonElement({id: 'carousel-navigation-dot-index-2'}), 2);
+});
+
+test('should calculate scroll amout for index', async t => {
+  const { rewiredScript } = t.context as any;
+  const calculateScrollPosition = rewiredScript.__get__('calculateScrollPosition');
+  t.is(calculateScrollPosition(0, 3, 30), 0);
+  t.is(calculateScrollPosition(1, 3, 30), 10);
+  t.is(calculateScrollPosition(2, 3, 30), 20);
 });
--- src/templates/interstitial-carousel-ad-unit/script.ts
+++ src/templates/interstitial-carousel-ad-unit/script.ts
@@ -1,10 +1,124 @@
-function func(a: number, b: number): number {
-  return a + b;
+interface CarouselElements {
+  readonly dotButtons: ReadonlyArray<HTMLButtonElement>;
+  readonly leftButton: HTMLButtonElement;
+  readonly rightButton: HTMLButtonElement;
+  readonly scrollableContainer: HTMLElement;
 }
 
+interface CarouselState {
+  readonly currentIndex: number;
+  readonly numberOfItems: number;
+}
+
+// tslint:disable-next-line:no-let
+let state: CarouselState;
+// tslint:disable-next-line:no-let
+let carouselElements: CarouselElements;
+
+function calculateTargetIndex(currentIndex: number, difference: number, numberOfItems: number): number {
+  const direction = (difference > 0) ? +1 : -1;
+  // tslint:disable-next-line:no-let
+  let newIndex = currentIndex;
+  // tslint:disable-next-line:no-let
+  for (let i = 0; i < Math.abs(difference); i++) {
+    newIndex = newIndex + direction;
+    if (newIndex < 0) {
+      newIndex = Math.max(0, numberOfItems - 1);
+    }
+    if (newIndex >= numberOfItems) {
+      newIndex = 0;
+    }
+  }
+  return newIndex;
+}
+
+function calculateScrollPosition(index: number, numberOfItems: number, scrollWidth: number): number {
+  return (scrollWidth / numberOfItems) * index;
+}
+
+function slideToIndex(index: number): void {
+  state = {...state,
+    currentIndex: index,
+  };
+  setActiveDotStyle();
+  slideToCurrentIndex();
+}
+
+function handleOnPreviousImageRequested(): void {
+  slideToIndex(calculateTargetIndex(state.currentIndex, -1, state.numberOfItems));
+}
+
+function handleOnNextImageRequested(): void {
+  slideToIndex(calculateTargetIndex(state.currentIndex, +1, state.numberOfItems));
+}
+
+function stripIndexFromDotButtonElement(button: HTMLButtonElement): number {
+  return Number(button.id.split('carousel-navigation-dot-index-')[1]);
+}
+
+function handleOnDotButtonClicked(e: MouseEvent): void {
+  const button = e.target as HTMLButtonElement;
+  slideToIndex(stripIndexFromDotButtonElement(button));
+}
+
+function setActiveDotStyle(): void {
+  carouselElements.dotButtons.forEach(button => {
+    if (stripIndexFromDotButtonElement(button) === state.currentIndex) {
+      button.classList.add('carousel-navigation-dot-active');
+    } else {
+      button.classList.remove('carousel-navigation-dot-active');
+    }
+  });
+}
+
+function slideToCurrentIndex(): void {
+  const targetScrollPosition = calculateScrollPosition(
+    state.currentIndex,
+    state.numberOfItems,
+    carouselElements.scrollableContainer.scrollWidth,
+  );
+  carouselElements.scrollableContainer.scrollTo({
+    behavior: 'smooth',
+    left: targetScrollPosition,
+    top: carouselElements.scrollableContainer.offsetTop,
+  });
+}
+
+function registerEventListeners(): void {
+  carouselElements.leftButton.addEventListener('click', handleOnPreviousImageRequested);
+  carouselElements.rightButton.addEventListener('click', handleOnNextImageRequested);
+  carouselElements.dotButtons.forEach((button) => button.addEventListener('click', handleOnDotButtonClicked));
+  window.addEventListener('resize', () => slideToCurrentIndex());
+}
+
+// Based on: https://gist.github.com/metasansana/beb3e06d423cfbf518eb565e4829ac28#file-debounce-ts
+const debounce = <A>(f: (a: A) => void, delay: number) => {
+  // tslint:disable-next-line:no-let
+  let timer: (number | null) = null;
+  return (a: A) => {
+    if (!timer) {
+      timer = Number(setTimeout(() => f(a), delay));
+    } else {
+      clearTimeout(timer);
+      timer = Number(setTimeout(() => f(a), delay));
+    }
+  };
+};
+
 window.document.addEventListener('DOMContentLoaded', () => {
-  // tslint:disable-next-line:no-console
-  console.log(func(1, 2));
+  carouselElements = {
+    dotButtons: Array.from(window.document.getElementsByClassName('carousel-navigation-dot')).map(el => el as HTMLButtonElement),
+    leftButton: window.document.getElementById('carousel-button-left') as HTMLButtonElement,
+    rightButton: window.document.getElementById('carousel-button-right') as HTMLButtonElement,
+    scrollableContainer: window.document.getElementById('carousel-container') as HTMLElement,
+  };
+  state = {
+    currentIndex: 0,
+    numberOfItems: carouselElements.dotButtons.length,
+  };
+  setActiveDotStyle();
+  registerEventListeners();
+  slideToCurrentIndex();
 });
 
 
